cfffba9ab39bdac38e58bd455e85605ea1b0ced1
Deodorant1 <97267207+Deodorant1@users.noreply.github.com>
2022-01-06 19:47:24 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Title = "What is your name";
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WindowHeight =40;
+
+Console.WriteLine("What is Your Name");
+
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello " + userName + ", nice to meet you!");
+
+double num01;
+double num02;
+
+Console.Write("input a number: ");
+num01 = Convert.ToDouble( Console.ReadLine() );
+
+Console.Write("input a second number: ");
+num02 = Convert.ToDouble( Console.ReadLine() );
+
+double result = num01 + num02 ;
+Console.WriteLine("The Average of These numbers is " + result/2);
+
+Console.ReadKey();
